b8c4a7c84d9fa37b209fcd24d3ddf11931f98d12
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-24 14:03:32 +0200

    impl header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
+import Header from "./Componenets/Header";
 import Login from "./Componenets/Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
