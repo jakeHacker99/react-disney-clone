1d651f2a88c7486a5f20b32decc3b4b05984b3e5
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-24 15:57:35 +0200

    impl redux store

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider state={store}></Provider>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
